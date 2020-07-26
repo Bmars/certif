5c1a8b2c880441e05fbda01e270720e0dbf91ba6
Bmars <bapt_13@hotmail.fr>
2020-07-26 15:42:45 +0200

    modifindex

--- index.php
+++ index.php
@@ -4,9 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="style.css">
-    <title>Document</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <title>accueil</title>
 </head>
 <body>
+    <header>Bonjour bienvenue sur notre espace membre</header>
+    <p>Veuillez choisir les options suivantes</p>
+    <button type="submit" class="btn btn-dark" name="submit"><a href="inscription.php">Inscription</a></button></br>
+    <button type="submit" class="btn btn-dark" name="submit"><a href="connection.php">connection</a></button>
     <div>
         <input type="checkbox" class="checkbox" id="chk" />
         <label class="label" for="chk">
